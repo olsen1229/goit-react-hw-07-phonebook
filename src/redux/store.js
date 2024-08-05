87d4ffc7c89091da55704a855d083a7b2ffea61c
olsen1229 <marylou.olsen7@gmail.com>
2024-08-05 21:00:56 +0800

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsSlice } from './contactsSlice';
 import { filterSlice } from './filterSlice';
 
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+//import { persistStore, persistReducer } from 'redux-persist';
+//import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 
 // Combine your reducers
@@ -15,23 +15,25 @@ const rootReducer = combineReducers({
 });
 
 // Create the persist config object
-const persistConfig = {
-  key: 'root',
-  storage,
+//const persistConfig = {
+//  key: 'root',
+ // storage,
   // You can specify which parts of your state you want to persist here
-  whitelist: ['contacts'], // In your case, you probably only want to persist contacts
-};
+//  whitelist: ['contacts'], // In your case, you probably only want to persist contacts
+//};
 
 // Wrap your rootReducer with persistReducer
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+//const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//export const store = configureStore({
+  //reducer: persistedReducer,
+  // The devTools enhancer are automatically included by default, so you don't need to specify them.
+ // middleware: getDefaultMiddleware =>
+  //  getDefaultMiddleware({
+  //    serializableCheck: false,
+ //   }),
+//});
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  // The devTools enhancer are automatically included by default, so you don't need to specify them.
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
-});
-
-export const persistor = persistStore(store);
+  reducer: rootReducer
+})
